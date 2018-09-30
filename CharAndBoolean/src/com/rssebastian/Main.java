4b990de504458588bb0d2281b7961acb5914ead8
Lol Cinnamonbuns <sebastiansryan@gmail.com>
2018-09-30 15:36:43 -0700

    char and boolean added

--- CharAndBoolean/src/com/rssebastian/Main.java
+++ CharAndBoolean/src/com/rssebastian/Main.java
@@ -1,8 +1,22 @@
 package com.rssebastian;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        char myChar = 'D';
+
+        //width of 16 (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = true;
+
+        // 1. Find the code for the registered symbol on the same line as the copyright symbol
+        // 2. Create a variable of type char and assign it the unicode value for that symbol
+        // 3. Display it on screen
+
+        char registered = '\u00AE';
+        System.out.println(registered);
     }
 }
