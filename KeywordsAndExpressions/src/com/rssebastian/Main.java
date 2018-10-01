ba7f709099bb225a13dbfb4f0cbbf72e0b8e69ca
Lol Cinnamonbuns <sebastiansryan@gmail.com>
2018-09-30 22:19:20 -0700

    expressions

--- KeywordsAndExpressions/src/com/rssebastian/Main.java
+++ KeywordsAndExpressions/src/com/rssebastian/Main.java
@@ -3,6 +3,14 @@ package com.rssebastian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // a mile is equal to 1.6890344 kilometers
+        double kilometers = (100 * 1.6890344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // Data type and brackets are not part of the expression
     }
 }
