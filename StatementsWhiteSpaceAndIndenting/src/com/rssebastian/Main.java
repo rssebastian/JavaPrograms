f4cd716ed44b70842ed0bc87e0e7cef88ba1f86e
Lol Cinnamonbuns <sebastiansryan@gmail.com>
2018-09-30 22:29:00 -0700

    whitespace review

--- StatementsWhiteSpaceAndIndenting/src/com/rssebastian/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/com/rssebastian/Main.java
@@ -3,6 +3,15 @@ package com.rssebastian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myVariable = 50;
+	    myVariable++;
+	    myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still here");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
     }
 }
